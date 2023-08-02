20f2cbb24ec79d13ade964d88d94db531a3853a0
ksc <ksc036@gmail.com>
2023-08-03 07:26:12 +0900

    publicPath설정

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: '/test/',
   transpileDependencies: true,
-  devServer: {
-    https: false, // HTTPS 사용하지 않도록 설정
-  },
 })
