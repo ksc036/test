d6455833ab49e3eb58b819e596e04016e7aa07e3
ksc <ksc036@gmail.com>
2023-08-03 05:45:00 +0900

    https false

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: false, // HTTPS 사용하지 않도록 설정
+  },
 })
